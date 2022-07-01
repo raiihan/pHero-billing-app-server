7e7df96b887b7238102c40784e505edcacf6568a
raiihan <s.siilavn@yahoo.com>
2022-07-01 17:44:28 +0600

    user login

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const bcrypt = require('bcrypt');
 
 require('dotenv').config();
 
@@ -10,7 +12,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
+function verifyJWToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.status(401).send({ message: 'Unathorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_KEY, (err, decoded) => {
+        if (err) {
+            res.status(403).send({ message: 'Forbidden Access' });
+        }
+        else {
+            req.decoded = decoded;
+            next();
+        }
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tjmuq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -21,11 +38,46 @@ async function run() {
     try {
         await client.connect();
         const billsCollection = client.db("billing-app").collection("bills");
+        const usersCollection = client.db("billing-app").collection("users");
 
-        app.get('/billing-list', async (req, res) => {
+        app.post('/api/registration', async (req, res) => {
+            const email = req.body.email;
+            const password = await bcrypt.hash(req.body.password, 10);
+
+            const userExist = await usersCollection.findOne({ email });
+            if (userExist) {
+                res.status(400).send({ message: 'User Already Exists' })
+            }
+            else {
+                const result = await usersCollection.insertOne({ email, password })
+                res.send(result)
+            }
+
+        })
+
+        app.post('/api/login', async (req, res) => {
+            const { email, password } = req.body;
+            const user = await usersCollection.findOne({ email });
+            const accessJWT = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_KEY, { expiresIn: '1d' });
+            if (user) {
+                if (await bcrypt.compare(password, user.password)) {
+                    res.send({ user, accessJWT });
+                }
+                else {
+                    res.status(400).send({ message: 'password does not match' })
+                }
+            }
+            else {
+                res.status(400).send({ message: 'Invalid Email' })
+            }
+            // console.log(user.password);
+        })
+
+        app.get('/billing-list', verifyJWToken, async (req, res) => {
             const page = parseInt(req.query.page);
             const count = parseInt(req.query.count);
             const search = req.query.search;
+            const query = {}
             let bills;
             if (page || count) {
                 bills = (await (billsCollection.find({
@@ -38,13 +90,7 @@ async function run() {
             }
             else {
 
-                bills = await billsCollection.find({
-                    "$or": [
-                        { name: { $regex: search, $options: '$i' } },
-                        { email: { $regex: search } },
-                        { phone: { $regex: search } }
-                    ]
-                }).toArray()
+                bills = await billsCollection.find(query).toArray()
             }
             res.send(bills)
         })
@@ -56,14 +102,13 @@ async function run() {
             res.send(bill);
         })
 
-        app.post('/add-billing', async (req, res) => {
+        app.post('/add-billing', verifyJWToken, async (req, res) => {
             const bill = req.body
-            console.log(bill);
             const result = await billsCollection.insertOne(bill)
             res.send(result)
         })
 
-        app.patch('/update-billing/:id', async (req, res) => {
+        app.patch('/update-billing/:id', verifyJWToken, async (req, res) => {
             const id = req.params.id;
             const bill = req.body;
             const filter = { _id: ObjectId(id) };
@@ -79,7 +124,7 @@ async function run() {
             res.send(updateBill);
         })
 
-        app.delete('/delete-billing/:id', async (req, res) => {
+        app.delete('/delete-billing/:id', verifyJWToken, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await billsCollection.deleteOne(query);
